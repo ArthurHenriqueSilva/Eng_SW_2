df1d5254b67ce6e66f30600d302688e12b19c813
ArthurHenriqueSIlva <arthurhenrique.contactus@gmail.com>
2024-03-22 22:54:08 -0300

    feat: starting first landing page.

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const DescriptionSection = () => {
+  return (
+    <div className="footer">
+      <h2>Produto Acadêmico</h2>
+      <p>
+        Esse sistema faz parte do projeto da disciplina de Engenharia de Software 2 - 
+        Universidade Federal de Sergipe - 
+        2023.2
+        </p>
+    </div>
+  );
+}
+
+export default DescriptionSection;
